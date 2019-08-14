977df0a1225b43ef77ab485efb8b739fbef6e324
Ulisses Melo <ulisses.melo.nascimento@gmail.com>
2019-08-14 11:31:09 -0300

    Init: Mobile app

---
+++ tindev/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import { View, Text } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text>Funcionando</Text>
+    </View>
+  );
+};
+
+export default App;
